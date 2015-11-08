1a2ff0950d25c845317865396425b4bb3160673a
jiashan <tangjiashan@zjup.net>
2015-11-08 09:08:20 +0800

    delete mmaster

--- conpot/emulators/mmaster/mclient.py
+++
@@ -1,9 +0,0 @@
-from pymodbus.client.sync import ModbusTcpClient
-import master
-class MClient(master.Master):
-
-
-    def connect(self):
-        for m in self.master.modbuses:
-            client = ModbusTcpClient('127.0.0.1')
-

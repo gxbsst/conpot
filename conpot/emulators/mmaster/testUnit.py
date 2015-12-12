1b7feb5817e44a65ea29f0105edc85c14e075b63
jiashan <tangjiashan@zjup.net>
2015-12-12 11:03:35 +0800

    mmaster liuliangji

--- conpot/emulators/mmaster/testUnit.py
+++ conpot/emulators/mmaster/testUnit.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
     # obj = root.get_child(["0:Objects", "0:MJGrooveRobot"])
     # res = obj.call_method("0:Start", 10,10)
 
-    obj = root.get_child(["0:Objects", "0:SSRobot"])
+    # obj = root.get_child(["0:Objects", "0:SSRobot"])
     # res = obj.call_method("0:MovingStart", 0,3)
 
-    res = obj.call_method("0:WeldingStart", 3)
+    # res = obj.call_method("0:WeldingStart", 3)
 
     # obj = root.get_child(["0:Objects", "0:SSRobot"])
     # res = obj.call_method("0:WeldingStart", 1)
@@ -28,6 +28,16 @@ if __name__ == "__main__":
 
     # obj = root.get_child(["0:Objects", "0:MJMoveRobot"])
     # res = obj.call_method("0:Start", 1)
-    print res
+    #getting a variable by path and setting its value attribute
+    # var = root.get_child(["0:Objects", "0:MJMoveRobot", "0:MagnetConfirm"])
+    # print var
+    # var.set_value(ua.Variant([23], ua.VariantType.Int64))
+
+    #subscribing to data change event to our variable
+    # handler = SubHandler()
+    # sub = client.create_subscription(500, handler)
+    # sub.subscribe_data_change(var)
+
+    # print res
 
     client.disconnect()
